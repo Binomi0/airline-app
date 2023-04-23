1229c5899161aff6cd4bb1ad23ec4a259004aae1
Binomio <onrubia78@gmail.com>
2023-04-24 00:32:40 +0200

    feat: update text

--- routes/gas/GasStatus.tsx
+++ routes/gas/GasStatus.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 import { Box, Typography, Grid, CircularProgress } from "@mui/material";
+import { stakingAddress } from "contracts/address";
+import { parseNumber } from "utils";
 import GasAvailable from "./GasAvailable";
 import GasDeposited from "./GasDeposited";
 import GasFarmed from "./GasFarmed";
-import { stakingAddress } from "../../contracts/address";
-import { parseNumber } from "../../utils";
 
 const GasStatus = () => {
   const { contract } = useContract(stakingAddress);
@@ -18,7 +18,8 @@ const GasStatus = () => {
     <Box my={10}>
       <Typography variant="h2">Gas Station</Typography>
       <Typography variant="h5" gutterBottom>
-        Before Start Checklist, deposit AIRL Token in staking.
+        Before Start Checklist, deposit AIRL Token in staking and start earning
+        gasoline.
       </Typography>
       <Typography paragraph>
         Available:{" "}
