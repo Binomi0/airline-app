03bb37a99910222e8f1596d9e586fb7b252b6361
Binomi0 <adolfo@onrubia.es>
2023-06-10 20:41:52 +0200

    Feat: Add analytics setup config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { CacheProvider, EmotionCache } from '@emotion/react'
-import { Router } from 'next/router'
+import { Router, useRouter } from 'next/router'
 import {
   ThirdwebProvider,
   coinbaseWallet,
@@ -26,6 +26,16 @@ import { MainProvider } from 'context/MainProvider'
 import createEmotionCache from '../src/createEmotionCache'
 import theme from '../src/theme'
 import '../styles/globals.css'
+import ReactGA from 'react-ga'
+import Script from 'next/script'
+
+ReactGA.initialize('G-GP0EC7LJ5N', {
+  debug: process.env['NODE_ENV'] === 'development',
+  testMode: process.env['NODE_ENV'] === 'development',
+  gaOptions: {
+    name: 'tracker1'
+  }
+})
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -37,8 +47,12 @@ export interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache } = props
   const [loading, setLoading] = React.useState(false)
+  const router = useRouter()
 
+  console.log('Environment => ')
   React.useEffect(() => {
+    ReactGA.pageview(router.pathname, ['tracker1'])
+
     const toggleLoading = (status: boolean) => () => setLoading(status)
 
     Router.events.on('routeChangeStart', toggleLoading(true))
@@ -48,49 +62,60 @@ export default function MyApp(props: MyAppProps) {
       Router.events.off('routeChangeStart', toggleLoading(true))
       Router.events.off('routeChangeComplete', toggleLoading(false))
     }
-  }, [])
+  }, [router])
 
   return (
-    <ThirdwebProvider
-      activeChain={Goerli}
-      supportedChains={[Goerli]}
-      authConfig={{
-        domain: process.env['NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN'] || '',
-        authUrl: '/api/auth'
-      }}
-      supportedWallets={[
-        smartWallet({
-          factoryAddress,
-          thirdwebApiKey: process.env['NEXT_PUBLIC_API_KEY'] || '',
-          gasless: true,
-          personalWallets: [
-            metamaskWallet(),
-            // coinbaseWallet(),
-            localWallet({ persist: true })
-          ]
-        }),
-        metamaskWallet(),
-        coinbaseWallet(),
-        walletConnect(),
-        safeWallet(),
-        localWallet({ persist: true })
-      ]}
-    >
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <meta name='viewport' content='initial-scale=1, width=device-width' />
-        </Head>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <ErrorBoundary>
-            <MainProvider>
-              <AppBar />
-              <Sidebar />
-            </MainProvider>
-            <Component loading={loading} />
-          </ErrorBoundary>
-        </ThemeProvider>
-      </CacheProvider>
-    </ThirdwebProvider>
+    <>
+      <ThirdwebProvider
+        activeChain={Goerli}
+        supportedChains={[Goerli]}
+        authConfig={{
+          domain: process.env['NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN'] || '',
+          authUrl: '/api/auth'
+        }}
+        supportedWallets={[
+          smartWallet({
+            factoryAddress,
+            thirdwebApiKey: process.env['NEXT_PUBLIC_API_KEY'] || '',
+            gasless: true,
+            personalWallets: [
+              metamaskWallet(),
+              // coinbaseWallet(),
+              localWallet({ persist: true })
+            ]
+          }),
+          metamaskWallet(),
+          coinbaseWallet(),
+          walletConnect(),
+          safeWallet(),
+          localWallet({ persist: true })
+        ]}
+      >
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta name='viewport' content='initial-scale=1, width=device-width' />
+          </Head>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <ErrorBoundary>
+              <MainProvider>
+                <AppBar />
+                <Sidebar />
+              </MainProvider>
+              <Component loading={loading} />
+            </ErrorBoundary>
+          </ThemeProvider>
+        </CacheProvider>
+      </ThirdwebProvider>
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-GP0EC7LJ5N');
+            `}
+      </Script>
+    </>
   )
 }
