e80f5b657c70fcae899c94c05294cc09f2205acc
Binomio <onrubia78@gmail.com>
2024-03-16 18:22:17 +0100

    Fix: Import missing dependency

--- routes/Live/components/MCDUView.tsx
+++ routes/Live/components/MCDUView.tsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
+import LinearProgress from '@mui/material/LinearProgress'
 // import MiniMap from './MiniMap'
 
 interface Props {
