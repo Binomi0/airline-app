6e18348164032f6db073c970ed558222436f322b
DESKTOP-U9NQFET\king_ <onrubia78@gmail.com>
2026-03-13 19:37:45 +0100

    refactor: update context providers to use new fetcher

--- context/AircraftProvider.tsx
+++ context/AircraftProvider.tsx
@@ -2,32 +2,36 @@ import React from 'react'
 import { nftAircraftTokenAddress } from 'contracts/address'
 import { useSetRecoilState } from 'recoil'
 import { aircraftNftStore, ownedAircraftNftStore } from 'store/aircraftNFT.atom'
-import { getContract } from 'thirdweb'
-import { sepolia } from 'thirdweb/chains'
-import { twClient } from 'components/CustomWeb3Provider'
-import useNFTs from 'hooks/useNFTs'
-import useOwnedNfts from 'hooks/useOwnedNFTs'
+import { fetcher } from 'utils'
+import useSWR from 'swr'
 
 interface Props {
   children: JSX.Element
 }
 
 export const AircraftProvider = ({ children }: Props) => {
-  const contract = getContract({ address: nftAircraftTokenAddress, chain: sepolia, client: twClient })
-  const { data: aircrafts } = useNFTs(contract.address)
-  const { data: ownedAircrafts } = useOwnedNfts(contract.address)
-  console.log({ ownedAircrafts, aircrafts })
-
+  const { data: nfts } = useSWR<any[]>('/api/nft', fetcher)
   const setAircraftNFTs = useSetRecoilState(aircraftNftStore)
-  const setOwnedAircrafts = useSetRecoilState(ownedAircraftNftStore)
+  const setOwnedAircraftsStore = useSetRecoilState(ownedAircraftNftStore)
+
+  const aircrafts = React.useMemo(() => {
+    if (!nfts) return []
+    return nfts.filter(
+      (nft: any) => nft.tokenAddress.toLowerCase() === nftAircraftTokenAddress.toLowerCase()
+    )
+  }, [nfts])
+
+  const ownedAircrafts = React.useMemo(() => {
+    return aircrafts.filter((nft: any) => nft.owner !== null)
+  }, [aircrafts])
 
   React.useEffect(() => {
     setAircraftNFTs(aircrafts)
   }, [aircrafts, setAircraftNFTs])
 
   React.useEffect(() => {
-    setOwnedAircrafts(ownedAircrafts)
-  }, [ownedAircrafts, setOwnedAircrafts])
+    setOwnedAircraftsStore(ownedAircrafts)
+  }, [ownedAircrafts, setOwnedAircraftsStore])
 
   return children
 }
--- context/LicenseProvider.tsx
+++ context/LicenseProvider.tsx
@@ -2,31 +2,36 @@ import React from 'react'
 import { nftLicenseTokenAddress } from 'contracts/address'
 import { useSetRecoilState } from 'recoil'
 import { licenseNftStore, ownedLicenseNftStore } from 'store/licenseNFT.atom'
-import { sepolia } from 'thirdweb/chains'
-import { twClient } from 'components/CustomWeb3Provider'
-import { getContract } from 'thirdweb'
-import useNFTs from 'hooks/useNFTs'
-import useOwnedNfts from 'hooks/useOwnedNFTs'
+import { fetcher } from 'utils'
+import useSWR from 'swr'
 
 interface Props {
   children: JSX.Element
 }
 
 export const LicenseProvider = ({ children }: Props) => {
-  const contract = getContract({ address: nftLicenseTokenAddress, chain: sepolia, client: twClient })
-  const { data: licenses } = useNFTs(contract.address)
-  const { data: ownedLicenses } = useOwnedNfts(contract.address)
+  const { data: nfts } = useSWR<any[]>('/api/nft', fetcher)
   const setLicenseNFTs = useSetRecoilState(licenseNftStore)
   const setOwnedLicenseNftStore = useSetRecoilState(ownedLicenseNftStore)
-  console.log({ ownedLicenses, licenses })
+
+  const licenseList = React.useMemo(() => {
+    if (!nfts) return []
+    return nfts.filter(
+      (nft: any) => nft.tokenAddress.toLowerCase() === nftLicenseTokenAddress.toLowerCase()
+    )
+  }, [nfts])
+
+  const ownedList = React.useMemo(() => {
+    return licenseList.filter((nft: any) => nft.owner !== null)
+  }, [licenseList])
 
   React.useEffect(() => {
-    setLicenseNFTs(licenses)
-  }, [licenses, setLicenseNFTs])
+    setLicenseNFTs(licenseList)
+  }, [licenseList, setLicenseNFTs])
 
   React.useEffect(() => {
-    setOwnedLicenseNftStore(ownedLicenses)
-  }, [ownedLicenses, setOwnedLicenseNftStore])
+    setOwnedLicenseNftStore(ownedList)
+  }, [ownedList, setOwnedLicenseNftStore])
 
   return children
 }
