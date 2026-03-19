1538b4e8298207207d3f934cb8e94b570869bd85
DESKTOP-U9NQFET\king_ <onrubia78@gmail.com>
2026-03-19 12:02:40 +0100

    Add vercel url to development deploys

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_ORIGIN },
+          { key: 'Access-Control-Allow-Origin', value: process.env.NEXT_PUBLIC_ORIGIN || process.env.VERCEL_URL },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
